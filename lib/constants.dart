d6a27524e800c527652d96462df540b1664d87c7
Milkessa-Oljira <milkessaoljira@gmail.com>
2021-10-19 17:59:08 +0300

    setup

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFFF2400);
+const kPrimaryLightColor = Color(0xFFFFC5F3);
+const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFFFFC5F3), Color(0xFFFF2400)],
+);
+const kSecondaryColor = Color(0xFF979797);
+const kTextColor = Color(0xFF757575);
+const kTextColorLight = Color(0xFF8B8B8B);
---
+++ lib/theme.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'constants.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    fontFamily: 'Muli',
+    appBarTheme: appBarTheme(),
+    textTheme: textTheme(),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    primarySwatch: Colors.blue,
+  );
+}
+
+AppBarTheme appBarTheme() {
+  return const AppBarTheme(
+    color: Colors.white,
+    elevation: 0,
+    systemOverlayStyle: SystemUiOverlayStyle.light,
+    iconTheme: IconThemeData(color: Colors.black),
+    titleTextStyle: TextStyle(color: kTextColorLight, fontSize: 18),
+    centerTitle: true,
+  );
+}
+
+TextTheme textTheme() {
+  return const TextTheme(
+    bodyText1: TextStyle(color: kTextColor),
+    bodyText2: TextStyle(color: kTextColor),
+  );
+}
